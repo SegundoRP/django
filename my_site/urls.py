24eb407aba4214ea430eecfe98f680bd07966198
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-16 00:27:07 -0500

    feat: :sparkles: add new route to the prinicpal folder

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first_attempt project.
+URL configuration for my_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hello)
 ]
