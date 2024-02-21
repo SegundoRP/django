d7e960acdaa6e79f81e4218238e892ca6b0586ad
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-20 23:52:26 -0500

    feat: :sparkles: methods for show info of records instead object

--- my_app/models.py
+++ my_app/models.py
@@ -4,7 +4,14 @@ from django.db import models
 class Project(models.Model):
     name = models.CharField(max_length=200)
 
+    """ method for show name of model in admin panel instead object """
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
