a8938c3406e1c405d852c4565c4bf0fcf4a96894
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-20 23:30:28 -0500

    feat: :sparkles: method for view to get records of database

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,13 @@ def index(request):
 
 def hello(request, username):
     return HttpResponse('Hello %s' % username)
+
+def projects(request):
+    records = list(Project.objects.values())
+    return JsonResponse(records, safe=False)
+
+def tasks(request, id):
+    # task = Task.objects.get(id=id)
+    # Below ins the same than above
+    task = get_object_or_404(Task, id=id)
+    return JsonResponse('Task: %s' % task.title, safe=False)
