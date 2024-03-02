a75c95a3242ac1b8b74d0de1e07aade9ba1c4ac1
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-03-01 22:14:58 -0500

    feat: :sparkles: add name for url

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('hello/<str:username>', views.hello),
-    path('projects/', views.projects),
+    path('projects/', views.projects, name="projects"), # otra forma de nombras a las rutas con el name
     path('tasks/<int:id>', views.tasks),
     path('create_task', views.create_task)
 ]
